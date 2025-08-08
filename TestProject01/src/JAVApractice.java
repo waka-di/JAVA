e78532327b51cfd19326f5985e9f25994909b28a
waka-di <miho.1225.miho@icloud.com>
2025-08-08 20:18:37 +0900

    save

---
+++ TestProject01/src/JAVApractice.java
@@ -0,0 +1,12 @@
+public class JAVApractice {
+
+	public static void main(String[] args) {
+		int  a = 20;
+if(a < 8) {
+			System.out.println ("A");}
+	else if(a == 8) {
+		System.out.println ("B");}
+	else {
+		System.out.println ("C");}
+	}
+}
